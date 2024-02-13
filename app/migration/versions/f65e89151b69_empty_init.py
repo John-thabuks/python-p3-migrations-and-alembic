52866bcf924d6b38f682b508982bd4283d43b827
John-thabuks <thabuksjohn@gmail.com>
2024-02-13 07:54:47 +0300

    Well understond

---
+++ app/migration/versions/f65e89151b69_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: f65e89151b69
+Revises: 
+Create Date: 2024-02-12 20:49:37.061941
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f65e89151b69'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
